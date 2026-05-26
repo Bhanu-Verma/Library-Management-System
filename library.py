9bdf95aa35cc649e0dcb0c33d58949129b552ded
Bhanu-Verma <bhanu.verma.cse22@itbhu.ac.in>
2026-05-26 15:23:53 +0530

    Fix implementation to store user ids

--- library.py
+++ library.py
@@ -4,9 +4,9 @@ class Library:
     def __init__(self, name):
         self.name = name
         self.inventory = Inventory()
-        self.users = []
+        self.users = {}
         print(f"'{name}' is now open................")
 
     def register_user(self, user):
-        self.users.append(user)
+        self.users[user.user_id] = user
         print(f"'{user.name}' is now registerd in '{self.name}'")
