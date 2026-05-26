5503db5f14d2c8700dbf7bbc03a73837dabd560b
Bhanu-Verma <bhanu.verma.cse22@itbhu.ac.in>
2026-05-26 19:20:31 +0530

    Fix id generation for user

--- user.py
+++ user.py
@@ -2,6 +2,6 @@ class User:
     _id_generator = 0
 
     def __init__(self, name):
-        self.user_id = self._id_generator
+        self.user_id = User._id_generator
         User._id_generator += 1
         self.name = name
