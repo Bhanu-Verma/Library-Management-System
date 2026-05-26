4c0aa7e5e1a14b97ee1d70884a7ad83bcbc9bdb9
Bhanu-Verma <bhanu.verma.cse22@itbhu.ac.in>
2026-05-26 15:16:17 +0530

    Add BookCategory enum

--- book.py
+++ book.py
@@ -1,3 +1,10 @@
+from enum import Enum
+
+class BookCategory(Enum):
+    SCIENCE = 1
+    LITERATURE = 2
+    FICTION = 3
+    THESIS = 4
 class Book:
     def __init__(self, isbn, title, author, category):
         self.isbn = isbn
